f77be4e9abd214c33cc058a11e997ea3360c7059
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-10-26 21:42:16 +0600

    added the carousel

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { useContext } from 'react';
-import { AuthContext } from '../UserContext/UserContext';
-
+import picture from '../images/javascript_logo.png';
+import img from '../images/python-hero.jpg';
+import pic from '../images/react-pic.png';
 const Home = () => {
-    const {user} = useContext(AuthContext)
     return (
         <div>
-            <h2> this is home:{user.email} </h2>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={picture}></img>
+          </div>
+          <div className="carousel-item">
+            <img src={img}></img>
+          </div>
+          <div className="carousel-item">
+           <img src={pic}></img>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
         </div>
     );
 };
